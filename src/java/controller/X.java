a739856c8a018d78d191c1b3450f09a335e209ea
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2024-08-30 12:02:47 +0530

    dsfds

--- src/java/controller/X.java
+++ src/java/controller/X.java
@@ -1,12 +1,13 @@
 package controller;
 
+import com.google.gson.Gson;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.User;
 
 @WebServlet(name = "X", urlPatterns = {"/X"})
 public class X extends HttpServlet {
@@ -14,8 +15,9 @@ public class X extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
    
-        String name = request.getParameter("name");
-        System.out.println(name);
+        Gson gson = new Gson();
+        User user = gson.fromJson(request.getReader(), User.class);
+        System.out.println(user.getName());
         
         response.getWriter().write("Hello");
     }
